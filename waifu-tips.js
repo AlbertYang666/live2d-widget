12d10a52c46a8544b30a0dd5e65ee81914f02000
AlbertYang <44716364+AlbertYang666@users.noreply.github.com>
2020-09-17 20:41:44 +0800

    Update waifu-tips.js

--- waifu-tips.js
+++ waifu-tips.js
@@ -75,9 +75,6 @@ function loadWidget(config) {
 			Live2D.captureName = "photo.png";
 			Live2D.captureFrame = true;
 		});
-		document.querySelector("#waifu-tool .fa-info-circle").addEventListener("click", () => {
-			open("https://github.com/albertyang666/live2d-widget");
-		});
 		document.querySelector("#waifu-tool .fa-times").addEventListener("click", () => {
 			localStorage.setItem("waifu-display", Date.now());
 			showMessage("愿你有一天能与重要的人重逢。", 2000, 11);
